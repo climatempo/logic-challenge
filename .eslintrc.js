70fec6e7a6a437f03b4c6e740423c86926c954e6
Leo <leobc@protonmail.com>
2023-03-09 11:33:09 -0300

    chore: Add linting in pipeline

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'standard'
+  ],
+  rules: {
+    'space-infix-ops': 'off',
+  },
+};
--- src/model-composition.js
+++ src/model-composition.js
@@ -1,4 +1,4 @@
-exports.modelComposition = function(model, period) {
+exports.modelComposition = function (model, period) {
   const compositions = []
 
   // Write the code here
