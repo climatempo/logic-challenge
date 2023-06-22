d3783449f862d4cbdae3d8fd11103d7914bf14b1
Lars Dehlwes <ldehlwes@ethz.ch>
2023-06-22 18:00:44 -0300

    Add functionality for string input of startDate and endDate

--- src/model-composition.js
+++ src/model-composition.js
@@ -2,6 +2,18 @@ exports.modelComposition = function (model, period) {
   const compositions = []
 
   // Write the code here
+  // Parse dates if necessary (period object may contain Date objects or parsable UTC datetime string)
+  let stringMode = false
+  let copyStartDate = period.startDate
+  let copyEndDate = period.endDate
+  if(!(period.startDate instanceof Date)){
+    copyStartDate = new Date(period.startDate)
+    stringMode = true
+  }
+  if(!(period.endDate instanceof Date)){
+    copyEndDate = new Date(period.endDate)
+    stringMode = true
+  }
   // Loop over entries in model object
   Object.entries(model).forEach(modelEntry => {
     // Separate model name and model offset in days
@@ -23,28 +35,72 @@ exports.modelComposition = function (model, period) {
     hypotheticalStartDate.setDate(hypotheticalStartDate.getDate() + parseInt(modelOffsetsSplit[0]));
     hypotheticalEndDate.setDate(hypotheticalEndDate.getDate() + parseInt(modelOffsetsSplit[1]));
     // Check if the desired period intersects with the hypothetical model period
-    if(hypotheticalStartDate < period.endDate && period.startDate < hypotheticalEndDate){
-      let modelPeriod = {}
-      // If period.startDate is bigger than or equal to the hypotheticalStartDate, select period.startDate as startDate
-      // <= equality is needed to satisfy the toBe condition of the tests, toStrictEqual or toMatchObject would in the test script would be another solution
-      if(hypotheticalStartDate <= period.startDate){
-        modelPeriod.startDate = period.startDate
+    if(stringMode){
+      if(hypotheticalStartDate < copyEndDate && copyStartDate < hypotheticalEndDate){
+        let modelPeriod = {}
+        if(hypotheticalStartDate <= copyStartDate){
+          modelPeriod.startDate = formatDate(copyStartDate)
+        }
+        else{
+          modelPeriod.startDate = formatDate(hypotheticalStartDate)
+        }
+        if(copyEndDate <= hypotheticalEndDate){
+          modelPeriod.endDate = formatDate(copyEndDate)
+        }
+        else{
+          modelPeriod.endDate = formatDate(hypotheticalEndDate)
+        }
+        // Create model period object and append it to the list
+        const modelComposition = {"model": modelName, "period": modelPeriod};
+        compositions.push(modelComposition);
       }
-      else{
-        modelPeriod.startDate = hypotheticalStartDate
+    }
+    else{
+      if(hypotheticalStartDate < period.endDate && period.startDate < hypotheticalEndDate){
+        let modelPeriod = {}
+        // If period.startDate is bigger than or equal to the hypotheticalStartDate, select period.startDate as startDate
+        // <= equality is needed to satisfy the toBe condition of the tests, toStrictEqual or toMatchObject would in the test script would be another solution
+        if(hypotheticalStartDate <= period.startDate){
+          modelPeriod.startDate = period.startDate
+        }
+        else{
+          modelPeriod.startDate = hypotheticalStartDate
+        }
+        // If period.endDate is smaller than or equal to the hypotheticalEndDate, select period.endDate as endDate
+        // <= equality is needed to satisfy the toBe condition of the tests, toStrictEqual or toMatchObject would in the test script would be another solution
+        if(period.endDate <= hypotheticalEndDate){
+          modelPeriod.endDate = period.endDate
+        }
+        else{
+          modelPeriod.endDate = hypotheticalEndDate
+        }
+        // Create model period object and append it to the list
+        const modelComposition = {"model": modelName, "period": modelPeriod};
+        compositions.push(modelComposition);
       }
-      // If period.endDate is smaller than or equal to the hypotheticalEndDate, select period.endDate as endDate
-      // <= equality is needed to satisfy the toBe condition of the tests, toStrictEqual or toMatchObject would in the test script would be another solution
-      if(period.endDate <= hypotheticalEndDate){
-        modelPeriod.endDate = period.endDate
-      }
-      else{
-        modelPeriod.endDate = hypotheticalEndDate
-      }
-      // Create model period object and append it to the list
-      const modelComposition = {"model": modelName, "period": modelPeriod};
-      compositions.push(modelComposition);
     }
   });
   return compositions
+}
+
+// function to pad number
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+// function to format date as requested
+function formatDate(date) {
+  return (
+    [
+      date.getUTCFullYear(),
+      padTo2Digits(date.getUTCMonth() + 1),
+      padTo2Digits(date.getUTCDate()),
+    ].join('-') +
+    ' ' +
+    [
+      padTo2Digits(date.getUTCHours()),
+      padTo2Digits(date.getUTCMinutes()),
+      padTo2Digits(date.getUTCSeconds()),
+    ].join(':')
+  );
 }
