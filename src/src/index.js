920f7076b3ee356c8a805211d8c4d683cf1f763b
Pacagnan <119533393+Pacagnan@users.noreply.github.com>
2023-06-23 16:23:58 -0300

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,38 @@
+const { addDays } = require('date-fns');
+const { modelComposition } = require('./model-composition');
+const { models } = require('./constants');
+
+const currentDate = new Date();
+
+const metPeriod = {
+  startDate: currentDate,
+  endDate: addDays(currentDate, 4)
+};
+
+const wrfPeriod = {
+  startDate: addDays(currentDate, 5),
+  endDate: addDays(currentDate, 14)
+};
+
+const cfsPeriod = {
+  startDate: addDays(currentDate, 15),
+  endDate: addDays(currentDate, 29)
+};
+
+const allPeriod = {
+  startDate: currentDate,
+  endDate: addDays(currentDate, 29)
+};
+
+console.log('METEOROLOGIST');
+console.log(modelComposition(models, metPeriod));
+console.log('');
+console.log('WRF');
+console.log(modelComposition(models, wrfPeriod));
+console.log('');
+console.log('CFS');
+console.log(modelComposition(models, cfsPeriod));
+console.log('');
+console.log('ALL');
+console.log(modelComposition(models, allPeriod));
+
