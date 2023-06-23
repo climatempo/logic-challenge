d60f3c084ea4828f75f99e6d4507b650daef17ea
Camila Wolter <camila0wolter@gmail.com>
2023-06-23 15:31:30 -0300

    feat: implementation of the modelComposition function

--- src/model-composition.js
+++ src/model-composition.js
@@ -1,50 +1,53 @@
 exports.modelComposition = function (model, period) {
-  const startDate = new Date(period.startDate.getTime());
-  const endDate = new Date(period.endDate.getTime());
+  const compositions = []
 
-  const modelKeys = Object.keys(model);
-  const composition = [];
+  // Checks if startDate and endDate are valid instances of Date and, if not, converts the date strings to Date objects
+  let startDate = period.startDate
+  let endDate = period.endDate
 
-  for (let i = 0; i < modelKeys.length; i++) {
-    const modelName = modelKeys[i];
-    const [startOffset, endOffset] = model[modelName].split(':');
-    
-    const modelStartDate = new Date(startDate);
-    modelStartDate.setDate(modelStartDate.getDate() + parseInt(startOffset));
+  startDate = startDate instanceof Date ? startDate : new Date(period.startDate)
+  endDate = endDate instanceof Date ? endDate : new Date(period.endDate)
 
-    const modelEndDate = new Date(startDate);
-    modelEndDate.setDate(modelEndDate.getDate() + parseInt(endOffset));
+  // Loop over entries in the model object
+  Object.entries(model).forEach(modelEntry => {
+    const [modelName, modelOffsets] = modelEntry
+    const modelOffsetsSplit = modelOffsets.split(':')
 
+    // Create Date objects to represent the hypothetical start and end dates within the desired period
+    const modelStartDate = new Date()
+    modelStartDate.setUTCHours(0, 0, 0, 0)
+    modelStartDate.setDate(modelStartDate.getDate() + parseInt(modelOffsetsSplit[0]))
 
-    if (modelStartDate <= endDate && modelEndDate >= startDate) {
-      const modelPeriod = {
-        startDate: modelStartDate,
-        endDate: modelEndDate
-      };
+    const modelEndDate = new Date()
+    modelEndDate.setUTCHours(23, 59, 59, 0)
+    modelEndDate.setDate(modelEndDate.getDate() + parseInt(modelOffsetsSplit[1]))
 
-      if (modelStartDate < startDate) {
-        modelPeriod.startDate = startDate;
+    // Check if the desired period intersects with the hypothetical model period
+    if (modelStartDate < endDate && modelEndDate > startDate) {
+      const modelPeriod = {}
+
+      // Determine the start date of the model period based on the intersection with the desired period
+      if (modelStartDate <= startDate) {
+        modelPeriod.startDate = startDate
+      } else {
+        modelPeriod.startDate = modelStartDate
       }
 
-      if (modelEndDate > endDate) {
-        modelPeriod.endDate = endDate;
+      // Determine the end date of the model period based on the intersection with the desired period
+      if (endDate <= modelEndDate) {
+        modelPeriod.endDate = endDate
+      } else {
+        modelPeriod.endDate = modelEndDate
       }
 
-      if (!composition[modelName]) {
-        composition[modelName] = [];
-      }
-
-      composition[modelName].push({
+      // Create model period object and append it to the list
+      const modelComposition = {
         model: modelName,
         period: modelPeriod
-      });
+      }
+      compositions.push(modelComposition)
     }
-  }
+  })
 
-  const result = [];
-  for (const modelName in composition) {
-    result.push(...composition[modelName]);
-  }
-
-  return result;
+  return compositions
 }
